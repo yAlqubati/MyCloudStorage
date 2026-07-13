12d8a432ec8a131a0146873fbfb1fe68d2c23a5e
yAlqubati <y.alqubati56@gmail.com>
2026-07-13 15:55:28 +0300

    updated CORS

--- MyCloudStorage/Program.cs
+++ MyCloudStorage/Program.cs
@@ -147,7 +147,7 @@ builder.Services.AddCors( options =>
 {
     options.AddPolicy("Frontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("https://mystorage.live")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
